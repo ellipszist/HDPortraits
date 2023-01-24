ad8ca274c6be9a3812ce8fb71475fefe2152498a
Wren <grahamgrasley@gmail.com>
2023-01-24 13:32:29 -0500

    refactoring

--- HDPortraits/Patches/DialogueBoxCtorPatch.cs
+++ HDPortraits/Patches/DialogueBoxCtorPatch.cs
@@ -1,22 +1,21 @@
 ﻿using HarmonyLib;
 using StardewValley.Menus;
-using System;
 using System.Collections.Generic;
 using System.Reflection;
 
 namespace HDPortraits.Patches
 {
-    [HarmonyPatch]
-    class DialogueBoxCtorPatch
-    {
-        public static IEnumerable<MethodBase> TargetMethods()
-        {
-            foreach (var ctor in AccessTools.GetDeclaredConstructors(typeof(DialogueBox)))
-                yield return ctor;
-        }
-        public static void Postfix(DialogueBox __instance)
-        {
-            DialoguePatch.Init(__instance);
-        }
-    }
+	[HarmonyPatch]
+	class DialogueBoxCtorPatch
+	{
+		public static IEnumerable<MethodBase> TargetMethods()
+		{
+			foreach (var ctor in AccessTools.GetDeclaredConstructors(typeof(DialogueBox)))
+				yield return ctor;
+		}
+		public static void Postfix(DialogueBox __instance)
+		{
+			DialoguePatch.Init(__instance);
+		}
+	}
 }
--- HDPortraits/Patches/ExitMenuPatch.cs
+++ HDPortraits/Patches/ExitMenuPatch.cs
@@ -1,26 +1,24 @@
 ﻿using HarmonyLib;
 using StardewValley;
-using StardewValley.Menus;
-using System;
 
 namespace HDPortraits.Patches
 {
-    [HarmonyPatch]
-    internal class ExitMenuPatch
-    {
-        [HarmonyPatch(typeof(Game1), "exitActiveMenu")]
-        [HarmonyPrefix]
-        internal static void PrefixExitMenu()
-        {
-            DialoguePatch.Finish();
-        }
+	[HarmonyPatch]
+	internal class ExitMenuPatch
+	{
+		[HarmonyPatch(typeof(Game1), "exitActiveMenu")]
+		[HarmonyPrefix]
+		internal static void PrefixExitMenu()
+		{
+			DialoguePatch.Finish();
+		}
 
-        [HarmonyPatch(typeof(Event), "cleanup")]
-        [HarmonyPostfix]
-        internal static void PostfixEventCleanup()
-        {
-            PortraitDrawPatch.NpcEventSuffixes.Value.Clear();
-            DialoguePatch.Finish();
-        }
-    }
+		[HarmonyPatch(typeof(Event), "cleanup")]
+		[HarmonyPostfix]
+		internal static void PostfixEventCleanup()
+		{
+			PortraitDrawPatch.NpcEventSuffixes.Value.Clear();
+			DialoguePatch.Finish();
+		}
+	}
 }
